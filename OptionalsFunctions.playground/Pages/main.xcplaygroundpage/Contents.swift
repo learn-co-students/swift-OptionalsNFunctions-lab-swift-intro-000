6d4fec2347930114a48b0ba998b542254d5c1184
willsputra <suryadiputrawilliam@gmail.com>
2017-03-04 21:55:17 +0800

    Answered questions

--- OptionalsFunctions.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ OptionalsFunctions.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@
  */
 var petName = "Scooter the Turtle 🐢"
 
-
+//String, Scoother The Turtle 🐢
 
 
 
@@ -27,7 +27,7 @@ var petName = "Scooter the Turtle 🐢"
  */
 var otherPetName: String?
 
-
+//Optional, nil
 
 
 
@@ -36,7 +36,8 @@ var otherPetName: String?
 /*: question3
  ### 3. What is the _current value_ of `otherPetName`?
  */
-
+print(otherPetName)
+//Optional(nil)
 
 
 
@@ -48,7 +49,7 @@ var otherPetName: String?
  ### 4. What is the type of the variable `thirdPetName` declared below? What is its value?
  */
 var thirdPetName: String? = nil
-
+//Optional, nil
 
 
 
@@ -61,7 +62,8 @@ var thirdPetName: String? = nil
  ### 5. Assign a value to `thirdPetName`. What type of values can you assign to `thirdPetName`? After assigning a value, what is its type?
  */
 // write your code here
-
+//String, nil, Optional
+thirdPetName = "Dogg"
 
 
 
@@ -75,7 +77,7 @@ var thirdPetName: String? = nil
  ### 6. Print `thirdPetName` to the console using Swift's `print()` function. What do you expect to see in the console?
  */
 // write your code here
-
+print(thirdPetName)
 
 
 
@@ -87,7 +89,9 @@ var thirdPetName: String? = nil
  ### 7. Write an if statement that will print _just_ the value of `thirdPetName` to the console, without all the **Optional(...)** stuff.
  */
 // write your code here
-
+if let thirdPetName = thirdPetName{
+    print(thirdPetName)
+}
 
 
 
@@ -99,7 +103,15 @@ var thirdPetName: String? = nil
  ### 8. Write a function called `printPetName` that takes a pet's name as a parameter. It should print the pet's name if the pet name exists, or "There is no pet name!" if the pet's name does not exist.
  */
 // write your code here
-
+func printPetName(petName: String?) {
+    if petName != nil {
+        if let petName = petName {
+            print(petName)
+        }
+    } else {
+        print("There is no pet name!")
+    }
+}
 
 
 
@@ -111,7 +123,8 @@ var thirdPetName: String? = nil
  ### 9. Call `printPetName` with `thirdPetName`. Then call it again with `otherPetName`. What do you expect to see in the console?
  */
  // write your code here
-
+printPetName(petName: thirdPetName)
+printPetName(petName: otherPetName)
 
 
 
@@ -123,7 +136,9 @@ var thirdPetName: String? = nil
  ### 10. Write a function called `minimum`. `minimum` should take a list of `Int`s and return the minimum one in the bunch. For now, don't deal with the possibility of an empty array; assume the array passed to `minimum` always has one item. What should the return type of `minimum` be?
  */
 // write your code here
-
+func minimum (bunch: [Int]) -> Int {
+    return bunch.min()!
+}
 
 
 
@@ -136,7 +151,7 @@ var thirdPetName: String? = nil
  */
 let values1 = [4, 12, 3, 2, 9, 14, 11]
 // write your code here
-
+minimum(bunch: values1)
 
 
 
@@ -156,12 +171,17 @@ let values2: [Int] = []
 
 
 
-
 /*: question13
  ### 13. Write a new function, `maybeMinimum`. It should handle cases in which the array passed to it is empty. If the array is empty, `nil` should be returned; otherwise, the smallest `Int` in the array should be returned. What should the return type of `maybeMinimum` be? (Note: So far, you haven't seen anything other than optional `String`s. Do you expect other optional types, such as `Int`s, to be handled differently?)
  */
 // write your code here
-
+func maybeMinimum(bunch2: [Int]) -> Int? {
+    if bunch2.isEmpty {
+        return nil
+    }
+    
+    return bunch2.min()
+}
 
 
 
@@ -174,7 +194,7 @@ let values2: [Int] = []
  ### 14. Call `maybeMinum` with `values1`. What do you expect the return value to be?
  */
 // write your code here
-
+maybeMinimum(bunch2: values1)
 
 
 
@@ -186,7 +206,7 @@ let values2: [Int] = []
  ### 15. Call `maybeMinum` with `values2`, an empty array. What do you expect the return value to be?
  */
 // write your code here
-
+maybeMinimum(bunch2: values2)
 
 
 
