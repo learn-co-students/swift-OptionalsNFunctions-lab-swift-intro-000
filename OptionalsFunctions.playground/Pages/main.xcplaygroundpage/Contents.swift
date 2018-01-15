dda087deb638059b164042719d145ba6e1f24d04
Sameh M AbuRadi <Sameh96m@icloud.com>
2018-01-15 15:30:26 +0300

    Add files via upload

--- OptionalsFunctions.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ OptionalsFunctions.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -18,6 +18,10 @@ var petName = "Scooter the Turtle 🐢"
 
 
 
+
+
+
+
 /*: question2
  ### 2. What is the type of the variable `otherPetName` declared below? What is its value?
  */
@@ -26,9 +30,16 @@ var otherPetName: String?
 
 
 
+
+
+
 /*: question3
  ### 3. What is the _current value_ of `otherPetName`?
  */
+
+
+
+
 
 
 
@@ -42,10 +53,20 @@ var thirdPetName: String? = nil
 
 
 
+
+
+
+
 /*: question5
  ### 5. Assign a value to `thirdPetName`. What type of values can you assign to `thirdPetName`? After assigning a value, what is its type?
  */
 // write your code here
+
+
+
+
+
+
 
 
 
@@ -58,10 +79,18 @@ var thirdPetName: String? = nil
 
 
 
+
+
+
+
 /*: question7
  ### 7. Write an if statement that will print _just_ the value of `thirdPetName` to the console, without all the **Optional(...)** stuff.
  */
 // write your code here
+
+
+
+
 
 
 
@@ -74,6 +103,10 @@ var thirdPetName: String? = nil
 
 
 
+
+
+
+
 /*: question9
  ### 9. Call `printPetName` with `thirdPetName`. Then call it again with `otherPetName`. What do you expect to see in the console?
  */
@@ -82,10 +115,18 @@ var thirdPetName: String? = nil
 
 
 
+
+
+
+
 /*: question10
  ### 10. Write a function called `minimum`. `minimum` should take a list of `Int`s and return the minimum one in the bunch. For now, don't deal with the possibility of an empty array; assume the array passed to `minimum` always has one item. What should the return type of `minimum` be?
  */
 // write your code here
+
+
+
+
 
 
 
@@ -99,11 +140,19 @@ let values1 = [4, 12, 3, 2, 9, 14, 11]
 
 
 
+
+
+
+
 /*: question12
  ### 12. Call your `minimum` function with the following empty array. What happens when you call your function?
  */
 let values2: [Int] = []
 // write your code here
+
+
+
+
 
 
 
@@ -116,10 +165,19 @@ let values2: [Int] = []
 
 
 
+
+
+
+
+
 /*: question14
  ### 14. Call `maybeMinum` with `values1`. What do you expect the return value to be?
  */
 // write your code here
+
+
+
+
 
 
 
@@ -132,6 +190,8 @@ let values2: [Int] = []
 
 
 
-/*:
- [Solution](solution)
- */
+
+
+
+
+//: Click [here](https://github.com/learn-co-curriculum/swift-OptionalsNFunctions-lab/blob/solution/OptionalsFunctions.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution on GitHub.
