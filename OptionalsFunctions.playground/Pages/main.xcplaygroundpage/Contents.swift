2ca4cbbb6cb31e224c2202be35964f7a7c35bf67
mkes99 <mike@formativewebsolutions.com>
2018-08-27 23:10:47 -0700

    Optional Functions

--- OptionalsFunctions.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ OptionalsFunctions.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -16,11 +16,7 @@
 var petName = "Scooter the Turtle 🐢"
 
 
-
-
-
-
-
+//String
 
 /*: question2
  ### 2. What is the type of the variable `otherPetName` declared below? What is its value?
@@ -28,7 +24,7 @@ var petName = "Scooter the Turtle 🐢"
 var otherPetName: String?
 
 
-
+// Optionsl String
 
 
 
@@ -37,7 +33,7 @@ var otherPetName: String?
  ### 3. What is the _current value_ of `otherPetName`?
  */
 
-
+// nil
 
 
 
@@ -49,13 +45,7 @@ var otherPetName: String?
  */
 var thirdPetName: String? = nil
 
-
-
-
-
-
-
-
+// Optional String, nil
 
 /*: question5
  ### 5. Assign a value to `thirdPetName`. What type of values can you assign to `thirdPetName`? After assigning a value, what is its type?
@@ -63,25 +53,19 @@ var thirdPetName: String? = nil
 // write your code here
 
 
+thirdPetName = "Bob"
 
-
-
-
-
-
-
+// String
+// Optional
 
 /*: question6
  ### 6. Print `thirdPetName` to the console using Swift's `print()` function. What do you expect to see in the console?
  */
 // write your code here
 
+print(thirdPetName!)
 
-
-
-
-
-
+// Bob
 
 /*: question7
  ### 7. Write an if statement that will print _just_ the value of `thirdPetName` to the console, without all the **Optional(...)** stuff.
@@ -89,23 +73,26 @@ var thirdPetName: String? = nil
 // write your code here
 
 
-
-
-
-
-
+if let thirdPetName = thirdPetName{
+    print(thirdPetName)
+}
 
 /*: question8
  ### 8. Write a function called `printPetName` that takes a pet's name as a parameter. It should print the pet's name if the pet name exists, or "There is no pet name!" if the pet's name does not exist.
  */
 // write your code here
 
+func printPetName(name: String?){
+    guard let name = name else {
+        print("There is no pet name!")
+        return
+    }
+    
+    print(name)
+    return
+}
 
-
-
-
-
-
+// This is the one I am questioning. and not sure if its handled correctly.
 
 /*: question9
  ### 9. Call `printPetName` with `thirdPetName`. Then call it again with `otherPetName`. What do you expect to see in the console?
@@ -113,22 +100,17 @@ var thirdPetName: String? = nil
  // write your code here
 
 
-
-
-
-
-
+printPetName(name: thirdPetName) // Bob
+printPetName(name: otherPetName) // nil
 
 /*: question10
  ### 10. Write a function called `minimum`. `minimum` should take a list of `Int`s and return the minimum one in the bunch. For now, don't deal with the possibility of an empty array; assume the array passed to `minimum` always has one item. What should the return type of `minimum` be?
  */
 // write your code here
 
-
-
-
-
-
+func minimum(numbers: [Int]?) -> [Int]{
+    return numbers!
+}
 
 
 /*: question11
@@ -138,10 +120,7 @@ let values1 = [4, 12, 3, 2, 9, 14, 11]
 // write your code here
 
 
-
-
-
-
+minimum(numbers: values1) // [4, 12, 3, 2, 9, 14, 11]
 
 
 /*: question12
@@ -151,11 +130,7 @@ let values2: [Int] = []
 // write your code here
 
 
-
-
-
-
-
+minimum(numbers: values2) // Return is nil
 
 /*: question13
  ### 13. Write a new function, `maybeMinimum`. It should handle cases in which the array passed to it is empty. If the array is empty, `nil` should be returned; otherwise, the smallest `Int` in the array should be returned. What should the return type of `maybeMinimum` be? (Note: So far, you haven't seen anything other than optional `String`s. Do you expect other optional types, such as `Int`s, to be handled differently?)
@@ -163,24 +138,21 @@ let values2: [Int] = []
 // write your code here
 
 
+func maybeMinimum(numbers: [Int]?) -> Int?{
+    guard let numbers = numbers else {
+        return nil
+    }
+    return numbers.min()
+}
 
-
-
-
-
-
+// Is it ok to return an optional? I would assume so. but just want to confirm. Also for guard do I need to set a bool? "guard let this = this != nil else {}" or can it work without that not equals?
 
 /*: question14
  ### 14. Call `maybeMinum` with `values1`. What do you expect the return value to be?
  */
 // write your code here
 
-
-
-
-
-
-
+maybeMinimum(numbers: values1) // 2
 
 /*: question15
  ### 15. Call `maybeMinum` with `values2`, an empty array. What do you expect the return value to be?
@@ -188,10 +160,6 @@ let values2: [Int] = []
 // write your code here
 
 
-
-
-
-
-
+maybeMinimum(numbers: values2) // nil
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-OptionalsNFunctions-lab/blob/solution/OptionalsFunctions.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution on GitHub.
